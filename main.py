55e33c05dcecc82ce3d68f9ebc68d69f4b4f50dd
Tirilc <131136955+Tirilc@users.noreply.github.com>
2024-02-13 13:34:15 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 import pygame as pg
 import sys
+from settings import *
+from sprites import *
 
+class Game:
+    def __init__(self):
+        # Initiere pygame
+        pg.init()
+
+        # Lager hovedvinduet
+        self.screen = pg.display.set_mode(SIZE)
+
+        # Lager en klokke
+        self.clock = pg.time.Clock()
+        
+        # Attributt som styrer om spillet skal kjøres
+        self.running = True
