6f682d2711ecd6bd42620c05e4b855316355e91e
emmaevensen <129280429+emmaevensen@users.noreply.github.com>
2024-02-13 13:34:37 +0100

    Add files via upload
    legger til settings fil, med basic settings

---
+++ settings.py
@@ -0,0 +1,26 @@
+# Konstanter
+WIDTH = 500  # Bredden til vinduet
+HEIGHT = 600 # Høyden til vinduet
+
+# Størrelsen til vinduet
+SIZE = (WIDTH, HEIGHT)
+
+# Frames Per Second (bilder per sekund)
+FPS = 60
+
+# Farger (RGB)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+LIGHTBLUE = (100, 100, 255)
+GREY = (142, 142, 142)
+LIGHTRED = (255, 100, 100)
+
+GRAVITY = 0.8
+
+# Innstillinger til spilleren
+PLAYER_WIDTH = 40
+PLAYER_HEIGHT = 60
+PLAYER_ACC = 0.5
+PLAYER_FRICTION = -0.12
