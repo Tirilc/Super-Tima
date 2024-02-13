d9f4a8426c3d78e43dc8f8a33cd05426544837a6
Tirilc <131136955+Tirilc@users.noreply.github.com>
2024-02-13 13:39:58 +0100

    Update main.py

--- main.py
+++ main.py
@@ -16,3 +16,111 @@ class Game:
         
         # Attributt som styrer om spillet skal kjøres
         self.running = True
+        
+        
+    # Metode for å starte et nytt spill
+    def new(self):
+        # Lager spiller-objekt
+        self.player = Player()
+        
+        #lager platformer
+        while len(platform_list)<7:
+            #lager en ny platform
+            new_platform=Platform(
+                random.randint(10,WIDTH-110),
+                random.randint(50, HEIGHT-210),
+                100,
+                20
+                
+            )
+            
+            safe=True
+            
+            #sjekker om den nye platformen kolliderer med de gamle
+            for p in platform_list:
+                if pg.Rect.colliderect(new_platform.rect, p.rect):
+                    safe=False
+                    break
+            if safe:
+                #legger i lista
+                platform_list.append(new_platform)
+            else:
+                print("platformen kolliderte, prøver på nytt")
+        
+        self.run()
+        
+    
+    # Metode som kjører spillet
+    def run(self):
+        # Game loop
+        self.playing = True
+        
+        while self.playing:
+            self.clock.tick(FPS)
+            self.events()
+            self.update()
+            self.draw()
+        
+        
+    # Metode som håndterer hendelser
+    def events(self):
+        # Går gjennom hendelser (events)
+        for event in pg.event.get():
+            # Sjekker om vi ønsker å lukke vinduet
+            if event.type == pg.QUIT:
+                if self.playing:
+                    self.playing = False
+                self.running = False # Spillet skal avsluttes
+                
+            if event.type == pg.KEYDOWN:
+                # Spilleren skal hoppe hvis vi trykker på mellomromstasten
+                if event.key == pg.K_SPACE:
+                    self.player.jump()
+    
+    # Metode som oppdaterer
+    def update(self):
+        self.player.update()
+        
+        #sjekker om vi faller
+        if self.player.vel[1] >0:
+            collide=False
+            
+            #sjekker om spilleren kolliderer med en platform
+            for p in platform_list:
+                if pg.Rect.colliderect(self.player.rect, p.rect):
+                    collide=True
+                    break
+                
+            if collide:
+                self.player.pos[1] = p.rect.y-PLAYER_HEIGHT
+                self.player.vel[1]=0
+            
+    
+    # Metode som tegner ting på skjermen
+    def draw(self):
+        # Fyller skjermen med en farge
+        self.screen.fill(WHITE)
+        
+        #tegner platofrmene
+        for p in platform_list:
+            self.screen.blit(p.image, (p.rect.x, p.rect.y))
+        
+        # Tegner spilleren
+        self.screen.blit(self.player.image, self.player.pos)
+        
+        # "Flipper" displayet for å vise hva vi har tegnet
+        pg.display.flip()
+    
+    
+    # Metode som viser start-skjerm
+    def show_start_screen(self):
+        pass
+        
+        
+# Lager et spill-objekt
+game_object = Game()
+
+# Spill-løkken
+while game_object.running:
+    # Starter et nytt spill
+    game_object.new()
