e0b4d7454e5731748f4fd6821a7491eb9eacd6b6
Tirilc <131136955+Tirilc@users.noreply.github.com>
2024-02-29 09:31:57 +0100

    Update sprites.py
    startskjerm

--- sprites.py
+++ sprites.py
@@ -69,5 +69,35 @@ class Castle:
         self.rect.x = x
         self.rect.y = y
 
+class Button:
+    def __init__(self, x, y, width, height, fg, bg, content, fontsize):
+        self.font = pg.font.SysFont('Poppins-Regular', fontsize)
+        
+        self.x=x
+        self.y=y
+        self.width=width
+        self.height=height
+        
+        self.fg=fg
+        self.bg =bg
+        
+        self.image=pg.Surface((self.width, self.height))
+        self.image.fill(self.bg)
+        self.rect = self.image.get_rect()
+        
+        self.rect.x=self.x
+        self.rect.y=self.y
+        
+        self.text=self.font.render(content, True, self.fg)
+        self.text_rect=self.text.get_rect(center=(self.width/2, self.height/2))
+        self.image.blit(self.text,self.text_rect)
+        
+    def is_pressed(self, pos, pressed):
+        if self.rect.collidepoint(pos):
+            if pressed[0]:
+                return True
+            return False
+        return False
+
 
 #player = Player()
